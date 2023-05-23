da54a29792b6bc79678f938017ce8433a1685c79
umme aiman <122363026+umme-aimany@users.noreply.github.com>
2023-05-23 22:55:35 +0500

    Add files via upload

---
+++ events _js_prac/index.js
@@ -0,0 +1,23 @@
+function button(){
+    alert("i practice events in js")
+}
+///////////////////
+function popup(nam){
+    alert("hellow"+nam)
+}
+/////////////////////////////////
+function pop(lala){
+    alert("hellow "+lala)
+}
+///////////////////////
+function mine(my){
+    alert("men "+my)
+}
+/////////////////////
+function prompt(info){
+
+}
+///////////////////
+function enter(friends){
+    alert("hellow"+friends)
+}
